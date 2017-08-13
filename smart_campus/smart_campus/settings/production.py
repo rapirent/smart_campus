aa884c37dca577ac7051189f7d91810db3be27c5
jeffwang0516 <jeff1jeffo@gmail.com>
2017-08-14 00:52:40 +0800

    Use local setting file instead

---
+++ smart_campus/smart_campus/settings/production.py
@@ -0,0 +1,18 @@
+from .base import *
+
+SECRET_KEY = get_env_variable('SECRET_KEY')
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': get_env_variable('POSTGRESQL_NAME'),
+        'USER': get_env_variable('POSTGRESQL_USER'),
+        'PASSWORD': get_env_variable('POSTGRESQL_PASSWORD'),
+        'HOST': get_env_variable('POSTGRESQL_HOST'),
+        'PORT':  get_env_variable('POSTGRESQL_PORT'),
+    },
+}
